eb537554f38434a586088c17c72e201fe2979d6c
Rittik Halder <112501258+forittik@users.noreply.github.com>
2024-10-04 20:00:39 +0530

    Update tool.py

--- tool.py
+++ tool.py
@@ -44,11 +44,10 @@ Suggest ways they can learn from each other and address their challenges collabo
 
 @st.cache_data
 def load_data():
-    # URL to the CSV file
     file_path = 'https://raw.githubusercontent.com/forittik/updated_soca_tool/refs/heads/main/Dummy_questions.csv'
-
-    # Load the data from the CSV file with the first row as the header
-    df = pd.read_csv(file_path, header=0)
+    
+    # Load the data from the CSV file without a header
+    df = pd.read_csv(file_path, header=None, encoding='ISO-8859-1')  # or try 'utf-16'
 
     return df
 
